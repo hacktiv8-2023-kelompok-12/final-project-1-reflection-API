afdfaadf7ffb2cd511b6ca17582722aa1b397c25
Trickman <53070989+ipungdwi@users.noreply.github.com>
2023-10-24 20:22:17 +0700

    Update hash.js - generate token

--- src/lib/hash.js
+++ src/lib/hash.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
 
 module.exports = {
     Hash: function (plain) {
@@ -6,5 +8,16 @@ module.exports = {
     },
     Compare: function (hash, real) {
         return bcrypt.compare(real, hash);
+    },
+    GenerateToken: function (payload, secretKey, options) {
+        return jwt.sign(payload, secretKey, options);
+    },
+    VerifyToken: function (token, secretKey, options) {
+        return jwt.verify(token, secretKey, options);
     }
 }
+// kalo Mau ditambah
+// const options = {
+//     expiresIn: '1h', // Contoh waktu kedaluwarsa 1 jam
+//     algorithm: 'HS256' // Contoh algoritma enkripsi
+//   };
